17f6e28c8ab53bb3e13d9bff230c749d94972267
auliarahmat <auliarahmat25599@gmail.com>
2022-07-03 14:02:59 +0700

    codelab2 belajar image dan listView

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -2,16 +2,19 @@
 
 import 'package:flutter/material.dart';
 
+const infoTextStyle = TextStyle(fontFamily: 'Oxygen');
+
 class DetailScreen extends StatelessWidget {
   const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
+      body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Image.asset('images/foto.png'),
             Container(
               margin: EdgeInsets.only(top: 16.0),
               child: Text(
@@ -19,7 +22,7 @@ class DetailScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Staatliches',
                 ),
               ),
             ),
@@ -32,21 +35,21 @@ class DetailScreen extends StatelessWidget {
                     children: const [
                       Icon(Icons.calendar_today),
                       SizedBox(height: 8.0),
-                      Text('Open Everyday'),
+                      Text('Open Everyday', style: infoTextStyle),
                     ],
                   ),
                   Column(
                     children: const [
                       Icon(Icons.access_time),
                       SizedBox(height: 8.0),
-                      Text('09:00 - 20:00'),
+                      Text('09:00 - 20:00', style: infoTextStyle),
                     ],
                   ),
                   Column(
                     children: const [
                       Icon(Icons.monetization_on),
                       SizedBox(height: 8.0),
-                      Text('RP 25.000'),
+                      Text('RP 25.000', style: infoTextStyle),
                     ],
                   ),
                 ],
@@ -59,7 +62,39 @@ class DetailScreen extends StatelessWidget {
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 16.0),
               ),
-            )
+            ),
+            SizedBox(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-s/0d/7c/59/70/farmhouse-lembang.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-w/13/f0/22/f6/photo3jpg.jpg'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(16.0),
+                      child: Image.network(
+                          'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
